00b31a5140850d34dd2f66af3d6a94a0ce305d49
Nicholas Thom <Nicholas@Nicholass-MacBook-Pro.local>
2020-02-20 12:44:36 -0600

    deal and sim one round

--- game.py
+++ game.py
@@ -1,56 +1,79 @@
 import pandas as pd
 import numpy as np
+import random
 
 class Card:
     def __init__(self, face, suit):
         self.face = face
         self.suit = suit
 
-def main():
-    c1 = Card("A", "H")
-    print(c1.face)
-
-main()
+    def __repr__(self):
+        return '{'+str(self.face)+' of '+str(self.suit)+'}'
 
 class Player:
-    def __init__(self, name, stack, position, cards, bet):
-        self.name = name
-        self.stack = stack
-        self.cards = cards
-        self.position = position
-        self.bet = bet
+    def __init__(self, name, stack):
+        self.name = None
+        self.stack = None
+        self.cards = []
+        self.position = None
+        self.bet = None
+     def __repr__(self):
+        return '{Player ' + str(self.name) + ' with hand ' + str(self.cards) + ' and stack '+ str(self.stack) + '}'
 
-def main():
-    p1 = Player(100)
-    print(p1.stack)
 
-main()
 
 class Deck:
     def __init__(self):
         self.all_cards = None # Turn this into a list of all 52 cards
     def create_cards(self):
-        
-    def deal_cards(self, cards):
+        l = []
+        for item in ["C", "D", "H", "S"]:
+            for i in range(1,14):
+                l.append(Card(i, item))
+        self.all_cards = l
 
 
 
-class Game:
-	def __init__(self, num_players, pot, small_blind, big_blind):
-		self.num_players = num_players
-		self.pot = pot
-		self.small_blind = small_blind
-		self.big_blind = big_blind
-		self.players  = None
-		self.deck = None
+    def choose_cards(self, val):
+        cards = random.sample(self.all_cards, val)
+        return cards
 
-	def make_players(self, num_players):
-		l = []
-		for i in range(num_players):
-			player = Player()
-			player.name = i 
-		    l.append(player)
-		return l
+
+
+class Poker_Game:
+
+    def __init__(self, num_players, pot, small_blind, big_blind):
+        self.num_players = num_players
+        self.pot = pot
+        self.small_blind = small_blind
+        self.big_blind = big_blind
+        self.cards_on_board = None
+        self.players  = None
+        self.deck = None
+
+    def make_players(self):
+        l = []
+        for i in range(self.num_players):
+            player = Player(i, pot/num_players)
+            l.append(player)
+        return l
+
+    def get_deck(self):
+        self.deck = Deck()
+        self.deck.create_cards()
+
+    def deal(self):
+        needed_cards = 5 + 2 * self.num_players
+        cards_for_round = self.deck.choose_cards(needed_cards)
+        for person in self.players:
+            person.cards.append(cards_for_round.pop())
+            person.cards.append(cards_for_round.pop())
+        self.cards_on_board = cards_for_round
+
+
+
+
+
 
 
 
