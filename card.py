0497430af855f77f65f7ee43d65cefd74e74d3cb
Lawrence Fu <lawrencefu@wifi-131-179-6-224.host.ucla.edu>
2020-02-19 21:40:26 -0800

    test

--- card.py
+++ card.py
@@ -1,1 +1,10 @@
-import os 
+class Card:
+    def __init__(self, face, suit):
+        self.face = face
+        self.suit = suit
+
+def main():
+    c1 = Card("A", "H")
+    print(c1.face)
+
+main()
---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, stack):
+        self.stack = stack;
+
+
+
+def main():
+    p1 = Player(100)
+    print(p1.stack)
+
+main()
